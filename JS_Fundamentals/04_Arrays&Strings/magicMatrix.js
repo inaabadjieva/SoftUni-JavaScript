6b1ee49f995fc004a807c8560d5a00308452e1ad
inaabadjieva <ina_abadjieva@yahoo.com>
2016-10-04 21:06:17 +0300

    update

--- JS_Fundamentals/04_Arrays&Strings/magicMatrix.js
+++ JS_Fundamentals/04_Arrays&Strings/magicMatrix.js
@@ -1,20 +1,28 @@
 function matrix(input){
 	let matrix = input.map(row => row.split(' ').map(Number))
 	let sum = input[0].split(' ').map(Number).reduce((a, b) => a+ b, 0)
-
-	for(let row = 0; row < matrix[0].length; row++){
-		let rowSum = matrix[row].map(Number).reduce((a, b) => a+ b, 0)
-		if(sum != rowSum) 
+	for(let row in input){
+		let rowSum = input[row].split(' ').map(Number).reduce((a, b) => a+ b, 0)
+		if(rowSum != sum){
 			return false
-		let colSum = 0
-		for(let col = 0; col < matrix[1].length; col++){
-			colSum += matrix[row][col]
 		}
-		if(sum != colSum) 
+	}
+	let calcSum = function(arr) {
+      return arr.reduce((a, b) => a + b, 0);
+   }
+    let colSums = matrix.map(function(row, i) {
+      return calcSum(matrix.map((row) => row[i]));
+    })
+	for(let s of colSums){
+		if(s != sum){
 			return false
+		}
 	}
 	return true
 }
 console.log(matrix(['4 5 6',
+'6 5 4',
+'5 5 5']))
+console.log(matrix(['7 5 6',
 '6 5 4',
 '5 5 5']))
