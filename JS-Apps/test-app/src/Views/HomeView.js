71845ee7b1ed19da6d02ce828e0787b76346bbd5
inaabadjieva <ina_abadjieva@yahoo.com>
2016-12-01 11:45:07 +0200

    update

---
+++ JS-Apps/test-app/src/Views/HomeView.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class HomeView extends Component {
+    render() {
+        return (
+            <div className="home-view">
+                <h1>Home</h1>
+                { this.props.username ?
+                    <p>Welcome, {this.props.username}.</p> :
+                    <p>Welcome to the book library.</p>
+                }
+            </div>
+        );
+    }
+}
+
+export default HomeView
